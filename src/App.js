1da00900b7e5d26cf2743fe064aff2bc237de6fd
JenniferCarmen <jennifercarmens@gmail.com>
2018-03-27 23:42:25 -0500

    login, sede

--- src/App.js
+++ src/App.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import Header from './components/Header';
-import firebase from 'firebase';
-import Login from './components/Login';
-import Footer from './components/Footer';
-import './App.css';
-
-class App extends Component {
-  constructor(props){
-    super(props);
-    var config = {
-      apiKey: "AIzaSyA0KGnze8Q2eeKLoxWNINrS-zuDe-Dzm5w",
-      authDomain: "telmexhub-5da16.firebaseapp.com",
-      databaseURL: "https://telmexhub-5da16.firebaseio.com",
-      projectId: "telmexhub-5da16",
-      storageBucket: "telmexhub-5da16.appspot.com",
-      messagingSenderId: "360942649813"
-    };
-    firebase.initializeApp(config);
-  }
-  
-  render() {
-    return (
-      <div className="App">
-        <Header/>
-        <Login db={ firebase }/>
-        <Footer/>
-      </div>
-    );
-  }
-=======
 import React from 'react';
 import Home from './components/home';
 import Sprints from './components/Sprints';
@@ -50,7 +18,6 @@ const App = (props) => {
 			</Switch>
 		</div>
 	</BrowserRouter>)
->>>>>>> 6195d725e4d5e287b672dd836c4ef5d23fb511c2
 }
 
 export default App;
