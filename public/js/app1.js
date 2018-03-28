2bf1398f55489dc8e2300fa1dfb412baede26b09
JenniferCarmen <jennifercarmens@gmail.com>
2018-03-28 08:10:15 -0500

    sprint

--- public/js/app1.js
+++ public/js/app1.js
@@ -1,49 +1,96 @@
+
 window.addEventListener('load', function() {
+  $('select').formSelect();
 
-  firebase.auth().onAuthStateChanged(function (user) {
-    var uiduser = user.uid;
-    firebase.database().ref('/TrainingManager/' + uiduser).on('value', snap => {
-      console.log(snap.val().sede);
-    })
-    console.log(uiduser);
-      if(user.uid)
-      var dbRefObject = firebase.database().ref();
-      organizar();
-  
-      function organizar() {
-        // sincronizar cambios
-        dbRefObject.on('value', function (snap) {
-          console.log(snap.val());
-        })
+  const numberSprint = 'sprint_1';
+  const dataTraining = () => {
+    firebase.auth().onAuthStateChanged(function(user) {
+      const uiduser = user.uid;
+
+      firebase.database().ref('/TrainingManager/' + uiduser).on('value', snap => {
+        const sede = snap.val().sede;
+        firebase.database().ref('/users/' + uiduser).on('value', snap => {
+          const nameTraining = snap.val().name;
+          $('#data').append(`<div>${sede}-${nameTraining}</div>`);
+        });
+      });
+    });
+  };
+  const fechaActual = () => {
+    var f = new Date();
+    $('#date').append(`${f.getDate()}/${f.getMonth() + 1}/${f.getFullYear()}</div>`);
+  };
+  const getSprintActual = () => {
+    firebase.database().ref('sprint').on('child_added', snapshot=> {
+      const estado = snapshot.val().estado;
+      if (estado == 1) {
+        $('#NumberSprint').append(`Sprint:${snapshot.val().number}`);
       }
-    
-  })
-  
-  $('#signOutBtna').click(function () {
-    firebase.auth().signOut().then(function () {
-      window.location.href = 'login.html';  
+    });
+  };
+ 
+  const getSquads = () => {
+    $('#sprint1').append(`<div>     
 
-    }).catch(function (error) {
+    <h4>Sprint1</h4></div>`);
+    firebase.database().ref('/Squads/' + numberSprint).on('child_added', snapshot=> {
+    
+      const idJedi = snapshot.val().jedi;
+      firebase.database().ref('/Jedi/' + idJedi).on('value', snap => {
+      
+        const jedi = snap.val().name;     
+        $('#sprint1').append(`<div>
+      <p>Squad: 
+      ${snapshot.val().name}</p>
+      <p>Jedi:${jedi}
+       </p>
+       <div id='jedi${snapshot.val().id}'>
+       </div>
+       <br/>
+      </div>
+      `);
+
+      if (idJedi=="Jedi000"){
+      getStudentsforSprint(0);
+      getStudentsforSprint(1);
+      getStudentsforSprint(2);
+      getStudentsforSprint(3);
+      getStudentsforSprint(4);
+
+      }
+    });
+
+  });
+
+};
+  const getStudentsforSprint = (numbersquad)=>{
+    firebase.database().ref('/Alumnas/').on('child_added', snapshot=> {
+
+      const nsquad = snapshot.val().squads.sprint_2;
+      
+      if (nsquad == numbersquad) {
+        $(`#jedi${numbersquad}`).append(`<div>
+        ${snapshot.val().name.first} ${snapshot.val().name.last}
+       
+        </div>
+        `);
+        
+      }
+  });
+}
+
+  // Llamado de funciones
+  dataTraining();
+  fechaActual();
+  getSprintActual();
+  getSquads();
+  $('#signOutBtna').click(function() {
+    firebase.auth().signOut().then(function() {
+      window.location.href = 'login.html';
+    }).catch(function(error) {
       alert(error.message);
     });
-  })
-
-    firebase.database().ref('Alumnas').on('value', function(snapshot) {
-        var html = '';
-      snapshot.forEach(function(e) {
-        var element = e.val();
-        var name = element.index;
-        var email = element.english;
-        html += '<li>' +
-        ' ' + name + ' </li>';
-      });
-      $('#data').append(html);
-    
-    });
-
-})
+  });
+});
 
 
-
-
-    
--- public/js/login.js
+++ public/js/login.js
@@ -1,68 +1,66 @@
-function getSprints() {
-  let starCountRef = firebase.database().ref('sprint-notes/');
-  console.log(starCountRef);
-}
-function saveUser(user) {
-  let userInfo = {
-    uid: user.uid,
-    name: "",
-    email: user.email,
-    foto: ""
-  }
-  firebase.database().ref("users/" + user.uid).set(userInfo);
+window.addEventListener('load', function () {
+  function saveUser(user) {
+    let userInfo = {
+      uid: user.uid,
+      name: "No definido",
+      email: user.email,
+      foto: ""
+    }
+    firebase.database().ref("users/" + user.uid).set(userInfo);
 
-  firebase.database().ref('TrainingManager/' + user.uid).set({
-    sede: "",
+    firebase.database().ref('TrainingManager/' + user.uid).set({
+      sede: "Lima",
+    });
+    window.location.href = 'pruebas.html';
+  }
+
+
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+
+      saveUser(user);
+      console.log("estas logueado");
+
+      var dialog = document.querySelector('#loginDialog');
+      if (!dialog.showModal) {
+        dialogPolyfill.registerDialog(dialog);
+      }
+      dialog.close();
+
+    } else {
+      // No user is signed in.
+      console.log("no estas logueado");
+      var dialog = document.querySelector('#loginDialog');
+      if (!dialog.showModal) {
+
+        dialogPolyfill.registerDialog(dialog);
+      }
+      dialog.showModal();
+    }
   });
+  /*LOGIN PROCESS*/
+  $('#loginBtn').click(
+    function () {
+      var email = $('.loginEmail').val();
+      var password = $('.loginPassword').val();
 
-}
-
-firebase.auth().onAuthStateChanged(function (user) {
-  if (user) {
-
-    saveUser(user);
-    console.log("estas logueado");
-
-    var dialog = document.querySelector('#loginDialog');
-    if (!dialog.showModal) {
-      dialogPolyfill.registerDialog(dialog);
+      if (email != "" && password != "") {
+        //$('#loginProgress').show();
+        $('#loginBtn').hide();
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+          M.toast({ html: error.message, classes: 'rounded' })
+          //$('#loginError').show().text(error.message);
+          //$('#loginProgress').hide();
+          $('#loginBtn').show();
+        })
+      }
     }
-    dialog.close();
-    window.location.href = 'pruebas.html';  
-
-  } else {
-    // No user is signed in.
-    console.log("no estas logueado");
-    var dialog = document.querySelector('#loginDialog');
-    if (!dialog.showModal) {
-
-      dialogPolyfill.registerDialog(dialog);
-    }
-    dialog.showModal();
-  }
+  );
+  /*LOGOUT PRECESS*/
+  $('#signOutBtna').click(function () {
+    firebase.auth().signOut().then(function () {
+    }).catch(function (error) {
+      alert(error.message);
+    });
+  })
 });
-/*LOGIN PROCESS*/
-$('#loginBtn').click(
-  function () {
-    var email = $('.loginEmail').val();
-    var password = $('.loginPassword').val();
-
-    if (email != "" && password != "") {
-      //$('#loginProgress').show();
-      $('#loginBtn').hide();
-      firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
-        M.toast({ html: error.message, classes: 'rounded' })
-        //$('#loginError').show().text(error.message);
-        //$('#loginProgress').hide();
-        $('#loginBtn').show();
-      })
-    }
-  }
-);
-/*LOGOUT PRECESS*/
-$('#signOutBtna').click(function () {
-  firebase.auth().signOut().then(function () {
-  }).catch(function (error) {
-    alert(error.message);
-  });
-})
